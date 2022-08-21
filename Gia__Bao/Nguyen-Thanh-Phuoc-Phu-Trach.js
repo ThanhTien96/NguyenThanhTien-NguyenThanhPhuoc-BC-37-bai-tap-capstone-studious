dbb4b53fb0886a8e31d5ddf096c2ca38927b592f
ThanhTien <thanhtien2094@gmail.com>
2022-08-21 20:18:11 +0700

    hoan thanh du an

---
+++ Gia__Bao/Nguyen-Thanh-Phuoc-Phu-Trach.js
@@ -0,0 +1,6 @@
+/**
+ * Nguyễn Thành Phước Phụ Trách
+ * 1. làm giao diện trang contact
+ * 2. làm giao diện trang blog posts
+ * 3. làm giao diện trang blog single
+ */
---
+++ Thanh__Tien/Nguyen_Thanh_Tien_Cong_viec_lam.js
@@ -0,0 +1,10 @@
+
+//  NGƯỜI KHỞI TẠO: NGUYỄN THANH TIẾN.
+
+//  NGÀY TẠO 14/08/2022
+
+
+//  phân công công việc trong dự án: 
+//  - xây dựng header và footer.
+//  - xây dựng trang index ( home );
+//  -xây dựng trang contact , about, courses , ladingpage , 404page. testing, deploy du an.
